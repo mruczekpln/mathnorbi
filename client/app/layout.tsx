b070d93e4cfdba500071667d42c6139a72dad735
mruczekpln <leonwdev@gmail.com>
2025-12-25 18:48:05 +0100

    build(deps): add shadcn to the project

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "mathnorbi.",
@@ -12,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={jetbrainsMono.variable}>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
